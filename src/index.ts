5babb7b5782b6e01b2ba00d0d6abb8f87fb71211
JuanE <juanernestogranados@gmail.com>
2022-08-15 19:17:10 -0500

    set up the basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 // main api file
+import express from 'express';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+//import { createServer } from 'http';
+
+const app = express();
+
+app.use(express.json()); // for parsing application/json
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on port ${PORT}`);
+});
